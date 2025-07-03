40430cbf122d9329e1b81604586016de6a16c3a5
Fiko <firuz0643@gmail.com>
2025-07-03 20:44:11 +0400

    work section at working checkpoint

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,20 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+
+
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 import "purecss/build/grids-min.css";
 import "purecss/build/grids-responsive-min.css";
 import "../sass/style.scss";
+
+
+const swiper = new Swiper('.swiper', {
+    // configure Swiper to use modules
+    modules: [Navigation, Pagination],
+    ...
+  });
