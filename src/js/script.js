cda8d04ba27b655890cb48c8137f8212b57ff236
fiko <firuz0643@gmail.com>
2025-07-10 13:13:53 +0400

    slider in work section is readt

--- src/js/script.js
+++ src/js/script.js
@@ -12,9 +12,32 @@ import "purecss/build/grids-min.css";
 import "purecss/build/grids-responsive-min.css";
 import "../sass/style.scss";
 
-
-const swiper = new Swiper('.swiper', {
-    // configure Swiper to use modules
+try {
+  swiper = new Swiper('.works__slider', {
+    slidesPerView: 1,
+    loop: true,
+    breakpoints: {
+      // when window width is >= 320px
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 5
+      },
+      // when window width is >= 480px
+      1920: {
+        slidesPerView: 3,
+        spaceBetween: 35
+      },
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".icon-right-open",
+      prevEl: ".icon-left-open",
+    },
     modules: [Navigation, Pagination],
-    ...
+
   });
+}
+catch (e) {}
